cc13533e0dc00dea1a6e9facbb4395a1243d4af6
24rush <iordache.alin@yahoo.com>
2023-06-01 17:48:52 +0300

    Added genq api route for vercel

---
+++ api/genq.ts
@@ -0,0 +1,50 @@
+/*export const config = {
+  runtime: "edge",
+};*/
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  if (req.method === 'OPTIONS') {
+    res.status(200).end()
+    return
+  }
+
+  const {prompt} = req.body;
+  /*
+  const { prompt } = (await req.json()) as {
+    prompt?: string;
+  };*/
+
+  const payload = {
+    model: "text-davinci-003",
+    prompt,
+    temperature: 0,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+    max_tokens: 200,
+    n: 1,
+    stop: ['#', ';']
+  };
+  /*
+    const response = await fetch("https://api.openai.com/v1/completions", {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY ?? ""}`,
+      },
+      method: "POST",
+      body: JSON.stringify(payload),
+    });
+  
+    const json = await response.json();
+*/
+  res.status(200).end(JSON.stringify(payload));    
+}
